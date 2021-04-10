fa4685c360c34a25682f9845ff1219171349e7c8
Marina <rakastettu13@gmail.com>
2021-04-10 03:19:55 +0000

    fixing problems

--- task_1_1.rb
+++ task_1_1.rb
@@ -1,29 +1,31 @@
 class Station
-  attr_accessor :train_list, :train_type_list
+  attr_accessor :train_list
   attr_reader :name
 
   def initialize(name)
     @name = name
-    self.train_list = []
-    self.train_type_list = { 'passenger' => 0, 'cargo' => 0 }
+    @train_list = []
   end
 
   def add_train(train)
-    train_list.push(train.number)
-    if train.type == 'passenger'
-      train_type_list['passenger'] += 1
-    else
-      train_type_list['cargo'] += 1
-    end
+    train_list.push(train)
   end
 
   def delete_train(train)
-    train_list.delete(train.number)
-    if train.type == 'passenger'
-      train_type_list['passenger'] -= 1
-    else
-      train_type_list['cargo'] -= 1
+    train_list.delete(train)
+  end
+
+  def train_type_list
+    passenger = 0
+    cargo = 0
+    train_list.each do |n|
+      if n.type == 'passenger'
+        passenger += 1
+      else
+        cargo += 1
+      end
     end
+    "passenger: #{passenger}, cargo: #{cargo}"
   end
 end
 
@@ -31,30 +33,25 @@ class Route
   attr_reader :station_list
 
   def initialize(first, last)
-    @station_list = [first.name, last.name]
+    @station_list = [first, last]
   end
 
   def add_station(new)
-    station_list.insert(station_list.size - 1, new.name)
+    station_list.insert(station_list.size - 1, new)
   end
 
   def delete_station(waste)
-    station_list.delete(waste.name)
+    station_list.delete(waste)
   end
 end
 
 class Train
-  attr_accessor :railcar, :speed, :station_list, :station_index,
-                :current_station, :previous_station, :next_station
+  attr_accessor :railcar, :speed, :station_index, :current_station, :route
   attr_reader :type, :number
 
-  def initialize(number, railcar, type = 0)
+  def initialize(number, railcar, type)
     @number = number
-    @type = if type.zero?
-              'passenger'
-            else
-              'cargo'
-            end
+    @type = type
     @railcar = railcar
     @speed = 0
   end
@@ -79,24 +76,27 @@ class Train
     end
   end
 
-  def go(route)
-    self.station_list = route.station_list
+  def go(new_route)
+    self.route = new_route
     self.station_index = 0
-    self.next_station = station_list[station_index + 1]
-    self.current_station = station_list[station_index]
+    self.current_station = route.station_list[station_index]
   end
 
   def go_ahead
     self.station_index += 1
-    self.next_station = station_list[station_index + 1]
-    self.previous_station = station_list[station_index - 1]
-    self.current_station = station_list[station_index]
+    self.current_station = route.station_list[station_index]
   end
 
   def go_back
     self.station_index -= 1
-    self.next_station = station_list[station_index + 1]
-    self.previous_station = station_list[station_index - 1]
-    self.current_station = station_list[station_index]
+    self.current_station = route.station_list[station_index]
+  end
+
+  def next_station
+    route.station_list[station_index + 1]
+  end
+
+  def previous_station
+    route.station_list[station_index - 1]
   end
 end
